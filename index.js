7a115d558ca830069622f6a6fd58ae7a2909f07b
Manish Kumar Yadav <manish.yadav@tweaking.local>
2026-03-23 11:35:16 +0530

    Cd pipeline setup code

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello DevOps 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
